f93c8b3067f665f98844824adf833015a1ca86e5
Beatriz <40210313@esmad.ipp.pt>
2024-10-07 19:45:10 +0100

    test

--- tdw-beatriz/__tests__/test.jsx
+++ tdw-beatriz/__tests__/test.jsx
@@ -1,20 +1,29 @@
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
+// import Home from "../src/pages/Home";
+// import "@testing-library/jest-dom";
+
+// test("renders Home component with correct content", () => {
+//   render(<Home />);
+//   const headingElement = screen.getByRole("heading", {
+//     name: /welcome to the home page!/i,
+//   });
+//   expect(headingElement).toBeInTheDocument();
+
+//   const paragraphElement = screen.getByText(
+//     /this is the home component of your application./i,
+//   );
+//   expect(paragraphElement).toBeInTheDocument();
+
+//   const buttonElement = screen.getByRole("button", { name: /get started/i });
+//   expect(buttonElement).toBeInTheDocument();
+// });
+
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Home from "../src/pages/Home";
-import "@testing-library/jest-dom";
 
-test("renders Home component with correct content", () => {
-  render(<Home />);
-  const headingElement = screen.getByRole("heading", {
-    name: /welcome to the home page!/i,
-  });
-  expect(headingElement).toBeInTheDocument();
-
-  const paragraphElement = screen.getByText(
-    /this is the home component of your application./i,
-  );
-  expect(paragraphElement).toBeInTheDocument();
-
-  const buttonElement = screen.getByRole("button", { name: /get started/i });
-  expect(buttonElement).toBeInTheDocument();
+test("renders MyComponent correctly", () => {
+  const { asFragment } = render(<Home />);
+  expect(asFragment()).toMatchSnapshot();
 });
